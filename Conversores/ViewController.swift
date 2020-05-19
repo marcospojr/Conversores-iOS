f72785a78f96931eb02e681ac08e15cd55cca45e
marcospojr <marcospojr13@gmail.com>
2020-05-19 08:20:10 -0300

    Preparando o projeto de conversores

--- Conversores/ViewController.swift
+++ Conversores/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UITextField!
+    @IBOutlet weak var lbResultUnit: UILabel!
+     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
 }
 
